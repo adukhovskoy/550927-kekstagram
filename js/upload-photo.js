fac625d4eb0b703a78836b71fcbc5e000ae0ec2f
Aleksandr Dukhovskoy <alexdoohee@gmail.com>
2018-12-09 17:58:01 +0600

    effects slider done

--- js/upload-photo.js
+++ js/upload-photo.js
@@ -2,6 +2,11 @@
 
 var ENTER_KEYCODE = 13;
 var ESCAPE_KEYCODE = 27;
+var effectLevelPin = document.querySelector('.effect-level__pin');
+var effectLevelLine = document.querySelector('.effect-level__line');
+var effectLevelDepth = document.querySelector('.effect-level__depth');
+var imageUploadPreview = document.querySelector('.img-upload__preview');
+var imageUploadPreviewImage = imageUploadPreview.querySelector('img');
 
 var showElement = function (selector) {
   var element = document.querySelector(selector);
@@ -73,8 +78,6 @@ var generateFilterPropertyValue = function (filterClassName, effectLevelPercent)
 };
 
 var applyImageFilterPreview = function (filterClassName) {
-  var imageUploadPreview = document.querySelector('.img-upload__preview');
-  var imageUploadPreviewImage = imageUploadPreview.querySelector('img');
   resetImageFilters(imageUploadPreview, imageUploadPreviewImage);
   var filterPropertyValue = generateFilterPropertyValue(filterClassName, calculateEffectLevelPercent());
   if (filterPropertyValue) {
@@ -92,36 +95,58 @@ document.querySelector('.img-upload__cancel').addEventListener('keydown', functi
   }
 });
 
-document.querySelector('.effect-level__pin').addEventListener('mouseup', function () {
+effectLevelPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startX = evt.clientX;
+  var overflowLeft = 0;
+  var overflowRight = 0;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shiftX = moveEvt.clientX - startX;
+    startX = moveEvt.clientX;
+    if ((overflowLeft >= 0) && (overflowRight <= 0)) {
+      effectLevelPin.style.left = (effectLevelPin.offsetLeft + shiftX) + 'px';
+      effectLevelDepth.style.width = (effectLevelPin.offsetLeft + shiftX) + 'px';
+    }
+    if (effectLevelPin.offsetLeft < 0) {
+      effectLevelPin.style.left = 0 + 'px';
+      effectLevelDepth.style.width = 0 + 'px';
+    } else if (effectLevelPin.offsetLeft > effectLevelLine.offsetWidth) {
+      effectLevelPin.style.left = effectLevelLine.offsetWidth + 'px';
+      effectLevelDepth.style.width = effectLevelLine.offsetWidth + 'px';
+    }
+
+    if (effectLevelPin.offsetLeft === 0) {
+      overflowLeft += shiftX;
+    } else if (parseInt(effectLevelPin.style.left, 10) === parseInt(effectLevelLine.offsetWidth, 10)) {
+      overflowRight += shiftX;
+    }
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    var selectedFilterName = document.querySelector('input[name="effect"]:checked').value;
+    applyImageFilterPreview('effects__preview--' + selectedFilterName);
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
   document.querySelector('.effect-level__value').value = calculateEffectLevelPercent();
 });
 
-document.querySelector('#effect-none').addEventListener('click', function () {
-  hideEffectSlider();
-  applyImageFilterPreview('effects__preview--none');
-});
-
-document.querySelector('#effect-chrome').addEventListener('click', function () {
-  showEffectSlider();
-  applyImageFilterPreview('effects__preview--chrome');
-});
-
-document.querySelector('#effect-sepia').addEventListener('click', function () {
-  showEffectSlider();
-  applyImageFilterPreview('effects__preview--sepia');
-});
-
-document.querySelector('#effect-marvin').addEventListener('click', function () {
-  showEffectSlider();
-  applyImageFilterPreview('effects__preview--marvin');
-});
-
-document.querySelector('#effect-phobos').addEventListener('click', function () {
-  showEffectSlider();
-  applyImageFilterPreview('effects__preview--phobos');
-});
-
-document.querySelector('#effect-heat').addEventListener('click', function () {
-  showEffectSlider();
-  applyImageFilterPreview('effects__preview--heat');
+document.querySelector('.effects__list').addEventListener('click', function (evt) {
+  if (evt.target.name === 'effect') {
+    showEffectSlider();
+    effectLevelPin.style.left = effectLevelLine.offsetWidth + 'px';
+    effectLevelDepth.style.width = effectLevelLine.offsetWidth + 'px';
+    var selectedFilterName = document.querySelector('input[name="effect"]:checked').value;
+    if (selectedFilterName === 'none') {
+      hideEffectSlider();
+    }
+    applyImageFilterPreview('effects__preview--' + selectedFilterName);
+  }
 });
